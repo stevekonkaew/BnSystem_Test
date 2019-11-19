3ec3e54044b69c7797a503ddbd0e23d84218d302
k.stiwie <k.stiwie@gmail.com>
2019-11-19 07:16:25 +0700

    Add first model

---
+++ BnSystem_Test/Models/Accounts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BnSystem_Test.Models
+{
+    public class Accounts
+    {
+        public int Id { get; set; }
+    }
+}
